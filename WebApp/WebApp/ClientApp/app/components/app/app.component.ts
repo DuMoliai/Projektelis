a1df0337e43be0bbd1d409c209f9097056bdf8a5
efkaldas <efkaldas@users.noreply.github.com>
2018-04-23 22:03:14 +0300

    Galima dėti žymes ant mapo

--- WebApp/WebApp/ClientApp/app/components/app/app.component.ts
+++ WebApp/WebApp/ClientApp/app/components/app/app.component.ts
@@ -6,8 +6,50 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    //Zoom level
+    zoom: number = 10;
+    //Map Position
     title: string = 'My map';
     lat: number = 54.8985207;
     lng: number = 23.90359650000005;
+
+    //Marker
+    markers: marker[] = [
+
+        {
+            name: 'asd',
+            lat: 54.8985207,
+            lng: 23.90359650000005,
+            draggable: false
+        }
+    ];
+
+    constructor() {
+
+    }
+
+    mapClicked($event: any) {
+        var newMarker = {
+            name: 'Untiled',
+            lat: $event.coords.lat,
+            lng: $event.coords.lng,
+            draggable: false
+        }
+        this.markers.push(newMarker);
+    }
+
+
+    clickedMarker(marker: marker, index: number) {
+        console.log('Clicked Marker:' + marker.name + 'at index' + index);
+
+    }
+
 }
+interface marker {
+    name?: string;
+    lat: number;
+    lng: number;
+    draggable: boolean;
+}
+
 
